af900589f8f9838f9a9a67a4c720f659f2ccf13d
litongjava <litongjava@qq.com>
2024-07-06 10:23:32 +0900

    release to v1.0.0

---
+++ src/main/java/com/litongjava/openai/tts/OpenAiTTSRequestVo.java
@@ -0,0 +1,16 @@
+package com.litongjava.openai.tts;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class OpenAiTTSRequestVo {
+  private String model, input, alloy;
+}
