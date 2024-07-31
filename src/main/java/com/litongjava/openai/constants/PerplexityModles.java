3a45b3248015d891eb7d14113f5dab7351599ca9
litongjava <litongjava@qq.com>
2024-07-31 17:15:28 +0900

    add llama 3.1 model

--- src/main/java/com/litongjava/openai/constants/PerplexityModles.java
+++ src/main/java/com/litongjava/openai/constants/PerplexityModles.java
@@ -5,7 +5,15 @@ public interface PerplexityModles {
   String llama_3_sonar_small_32k_online = "llama--sonar-small-32k-online";
   String llama_3_sonar_large_32k_chat = "llama-3-sonar-large-32k-chat";
   String llama_3_sonar_large_32k_online = "llama-3-sonar-large-32k-online";
+  String llama_3_1_sonar_small_128k_online = "llama-3.1-sonar-small-128k-online";
+  String llama_3_1_sonar_small_128k_chat = "llama-3.1-sonar-small-128k-chat";
+  String llama_3_1_sonar_large_128k_online = "llama-3.1-sonar-large-128k-online";
+  String llama_3_1_sonar_large_128k_chat = "llama-3.1-sonar-large-128k-chat";
+  
   String llama_3_8b_instruct = "llama-3-8b-instruct";
   String llama_3_70b_instruct = "llama-3-70b-instruct";
   String mixtral_8x7b_instruct = "mixtral-8x7b-instruct";
+  String llama_3_1_8b_instruct = "llama-3.1-8b-instruct";
+  String llama_3_1_70b_instruct = "llama-3.1-70b-instruct";
+
 }
