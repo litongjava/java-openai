7e32a680972661d3dda0b0eb3844be4a6773b676
litongjava <litongjava@qq.com>
2024-12-27 00:14:22 -1000

    update delta

---
+++ src/main/java/com/litongjava/openai/chat/ChatFunctionCall.java
@@ -0,0 +1,13 @@
+package com.litongjava.openai.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatFunctionCall {
+  private String name;
+  private String arguments;
+}
--- src/main/java/com/litongjava/openai/chat/Delta.java
+++ src/main/java/com/litongjava/openai/chat/Delta.java
@@ -8,5 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Delta {
+  private String role;
   private String content;
+  private ChatFunctionCall function_call;
+  private String refusal;
 }
