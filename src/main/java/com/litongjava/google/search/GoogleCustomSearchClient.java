5963faf4285f03140fc242b710cdc9551fd420e6
litongjava <litongjava@qq.com>
2025-01-10 21:21:41 -1000

    add GoogleCustomSearchClient

--- src/main/java/com/litongjava/google/search/GoogleCustomSearchClient.java
+++ src/main/java/com/litongjava/google/search/GoogleCustomSearchClient.java
@@ -7,13 +7,18 @@ import com.litongjava.tio.utils.url.UrlUtils;
 
 public class GoogleCustomSearchClient {
 
-  public static final String SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key=%s&=%s&q=%s";
+  public static final String SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q=%s";
 
   public static GoogleCustomSearchResponse search(String key, String cx, String q) {
     String encode = UrlUtils.encode(q);
     String url = String.format(SEARCH_URL, key, cx, encode);
     ResponseVo responseVo = Http.get(url);
-    String bodyString = responseVo.getBodyString();
-    return JsonUtils.parse(bodyString, GoogleCustomSearchResponse.class);
+    if (responseVo.isOk()) {
+      String bodyString = responseVo.getBodyString();
+      return JsonUtils.parse(bodyString, GoogleCustomSearchResponse.class);
+    } else {
+      throw new RuntimeException("request:" + url + " " + "response:" + responseVo.getBodyString());
+    }
+
   }
 }
