0ae1bdc8551cbd7549ecaedada1efb57819001eb
litongjava <litongjava@qq.com>
2025-02-21 22:58:18 -1000

    add support files

--- src/main/java/com/litongjava/groq/GropConst.java
+++ src/main/java/com/litongjava/groq/GropConst.java
@@ -1,6 +1,13 @@
 package com.litongjava.groq;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface GropConst {
 
   String API_BASE = "https://api.groq.com";
+
+  String[] SUPPORT_EXTS = { "flac", "mp3", "mp4", "mpeg", "mpga", "m4a", "ogg", "wav", "webm" };
+
+  List<String> SUPPORT_LIST = Arrays.asList(SUPPORT_EXTS);;
 }
