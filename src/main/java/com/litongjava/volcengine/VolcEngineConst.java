a910d5f11ef9f7ddc8e62a760b713e7fcfdf82a7
litongjava <litongjava@qq.com>
2025-02-24 17:48:17 -1000

    add VolcEngineModels

---
+++ src/main/java/com/litongjava/volcengine/VolcEngineConst.java
@@ -0,0 +1,5 @@
+package com.litongjava.volcengine;
+
+public class VolcEngineConst {
+  String BASE_URL = "https://ark.cn-beijing.volces.com";
+}
---
+++ src/main/java/com/litongjava/volcengine/VolcEngineModels.java
@@ -0,0 +1,8 @@
+package com.litongjava.volcengine;
+
+public interface VolcEngineModels {
+  String DEEPSEEK_V3_241226 = "deepseek-v3-241226";
+  String DEEPSEEK_R1_250120 = "deepseek-r1-250120";
+  String DEEPSEEK_CHAT = "deepseek-r1-distill-qwen-7b-250120";
+  String DEEPSEEK_REASONER = "deepseek-r1-distill-qwen-32b-250120";
+}
