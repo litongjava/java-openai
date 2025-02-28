7c15dbcbed86c5a8637cd20e47cf40956795e796
litongjava <litongjava@qq.com>
2025-02-28 10:16:40 -1000

    update EmbeddingRequestVo

--- src/main/java/com/litongjava/openai/embedding/EmbeddingRequestVo.java
+++ src/main/java/com/litongjava/openai/embedding/EmbeddingRequestVo.java
@@ -1,5 +1,7 @@
 package com.litongjava.openai.embedding;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class EmbeddingRequestVo {
-  private String input, model;
+  private String model;
+  private Object input;
 
   public EmbeddingRequestVo input(String input) {
     this.input = input;
@@ -18,5 +21,20 @@ public class EmbeddingRequestVo {
   public EmbeddingRequestVo model(String model) {
     this.model = model;
     return this;
+  }
+
+  public EmbeddingRequestVo(String input) {
+    this.input = input;
+  }
+
+  public EmbeddingRequestVo(List<String> input) {
+    this.input = input;
+  }
+
+  public String getInputStr() {
+    if (input != null) {
+      return (String) input;
+    }
+    return null;
   }
 }
