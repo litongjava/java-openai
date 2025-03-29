cc20c8c9448473ef9bd48513e6d4b01d992df3ef
litongjava <litongjava@qq.com>
2025-03-29 01:23:38 -1000

    update process result

--- src/main/java/com/litongjava/linux/ProcessResult.java
+++ src/main/java/com/litongjava/linux/ProcessResult.java
@@ -10,10 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProcessResult {
+  private long taskId;
   private int exitCode;
   private String executeCode;
   private String stdOut;
   private String stdErr;
+  private String output;
   private List<String> images;
   private List<String> videos;
 }
