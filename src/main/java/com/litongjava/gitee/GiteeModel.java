4efaf6713c1bbc23ed0443ad271358965e051f4f
Tong Li <litongjava001@gmail.com>
2025-05-08 16:51:37 -1000

    add Qwen3_8B

--- src/main/java/com/litongjava/gitee/GiteeModel.java
+++ src/main/java/com/litongjava/gitee/GiteeModel.java
@@ -2,4 +2,6 @@ package com.litongjava.gitee;
 
 public interface GiteeModel {
   String QWEN2_7B_INSTRUCT = "Qwen2-7B-Instruct";
+  String Qwen3_30B_A3B = "Qwen3-30B-A3B";
+  String Qwen3_8B = "Qwen3-8B";
 }
