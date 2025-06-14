5d4ad7d5a1da4ec745b18d3693a6a78b2920fdd1
Tong Li <litongjava001@gmail.com>
2025-06-14 11:58:43 -1000

    add QwenAiModels

---
+++ src/main/java/com/litongjava/qwen/QwenAiModels.java
@@ -0,0 +1,136 @@
+package com.litongjava.qwen;
+
+public interface QwenAiModels {
+  String QVQ_MAX = "qvq-max";
+  String QVQ_MAX_LATEST = "qvq-max-latest";
+  String QVQ_MAX_2025_03_25 = "qvq-max-2025-03-25";
+
+  String QWEN_VL_MAX = "qwen-vl-max";
+  String QWEN_VL_MAX_LATEST = "qwen-vl-max-latest";
+  String QWEN_VL_MAX_2025_04_08 = "qwen-vl-max-2025-04-08";
+  String QWEN_VL_MAX_2025_04_02 = "qwen-vl-max-2025-04-02";
+  String QWEN_VL_MAX_2025_01_25 = "qwen-vl-max-2025-01-25";
+  String QWEN_VL_MAX_2024_12_30 = "qwen-vl-max-2024-12-30";
+  String QWEN_VL_MAX_2024_11_19 = "qwen-vl-max-2024-11-19";
+  String QWEN_VL_MAX_2024_10_30 = "qwen-vl-max-2024-10-30";
+  String QWEN_VL_MAX_2024_08_09 = "qwen-vl-max-2024-08-09";
+
+  String QWEN_VL_PLUS = "qwen-vl-plus";
+  String QWEN_VL_PLUS_LATEST = "qwen-vl-plus-latest";
+  String QWEN_VL_PLUS_2025_01_25 = "qwen-vl-plus-2025-01-25";
+  String QWEN_VL_PLUS_2025_01_02 = "qwen-vl-plus-2025-01-02";
+  String QWEN_VL_PLUS_2024_08_09 = "qwen-vl-plus-2024-08-09";
+  String QWEN_VL_PLUS_2023_12_01 = "qwen-vl-plus-2023-12-01";
+
+  String QVQ_72B_PREVIEW = "qvq-72b-preview";
+
+  String QWEN2_5_VL_72B_INSTRUCT = "qwen2.5-vl-72b-instruct";
+  String QWEN2_5_VL_32B_INSTRUCT = "qwen2.5-vl-32b-instruct";
+  String QWEN2_5_VL_7B_INSTRUCT = "qwen2.5-vl-7b-instruct";
+  String QWEN2_5_VL_3B_INSTRUCT = "qwen2.5-vl-3b-instruct";
+
+  String QWEN2_VL_72B_INSTRUCT = "qwen2-vl-72b-instruct";
+  String QWEN2_VL_7B_INSTRUCT = "qwen2-vl-7b-instruct";
+  String QWEN2_VL_2B_INSTRUCT = "qwen2-vl-2b-instruct";
+
+  String QWEN_VL_V1 = "qwen-vl-v1";
+  String QWEN_VL_CHAT_V1 = "qwen-vl-chat-v1";
+
+  String QWEN_LONG = "qwen-long";
+
+  String QWQ_PLUS = "qwq-plus";
+  String QWQ_PLUS_LATEST = "qwq-plus-latest";
+  String QWQ_PLUS_2025_03_05 = "qwq-plus-2025-03-05";
+
+  String QWEN_MAX = "qwen-max";
+  String QWEN_MAX_LATEST = "qwen-max-latest";
+  String QWEN_MAX_2025_01_25 = "qwen-max-2025-01-25";
+  String QWEN_MAX_2024_09_19 = "qwen-max-2024-09-19";
+  String QWEN_MAX_2024_04_28 = "qwen-max-2024-04-28";
+  String QWEN_MAX_2024_04_03 = "qwen-max-2024-04-03";
+
+  String QWEN_PLUS = "qwen-plus";
+  String QWEN_PLUS_LATEST = "qwen-plus-latest";
+  String QWEN_PLUS_2025_04_28 = "qwen-plus-2025-04-28";
+  String QWEN_PLUS_2025_01_25 = "qwen-plus-2025-01-25";
+  String QWEN_PLUS_2025_01_12 = "qwen-plus-2025-01-12";
+  String QWEN_PLUS_2024_11_27 = "qwen-plus-2024-11-27";
+  String QWEN_PLUS_2024_11_25 = "qwen-plus-2024-11-25";
+  String QWEN_PLUS_2024_09_19 = "qwen-plus-2024-09-19";
+  String QWEN_PLUS_2024_08_06 = "qwen-plus-2024-08-06";
+  String QWEN_PLUS_2024_07_23 = "qwen-plus-2024-07-23";
+
+  String QWEN_TURBO = "qwen-turbo";
+  String QWEN_TURBO_LATEST = "qwen-turbo-latest";
+  String QWEN_TURBO_2025_04_28 = "qwen-turbo-2025-04-28";
+  String QWEN_TURBO_2025_02_11 = "qwen-turbo-2025-02-11";
+  String QWEN_TURBO_2024_11_01 = "qwen-turbo-2024-11-01";
+  String QWEN_TURBO_2024_09_19 = "qwen-turbo-2024-09-19";
+  String QWEN_TURBO_2024_06_24 = "qwen-turbo-2024-06-24";
+
+  String QWEN_MATH_PLUS = "qwen-math-plus";
+  String QWEN_MATH_PLUS_LATEST = "qwen-math-plus-latest";
+  String QWEN_MATH_PLUS_2024_09_19 = "qwen-math-plus-2024-09-19";
+  String QWEN_MATH_PLUS_2024_08_16 = "qwen-math-plus-2024-08-16";
+
+  String QWEN_MATH_TURBO = "qwen-math-turbo";
+  String QWEN_MATH_TURBO_LATEST = "qwen-math-turbo-latest";
+  String QWEN_MATH_TURBO_2024_09_19 = "qwen-math-turbo-2024-09-19";
+
+  String QWEN_CODER_PLUS = "qwen-coder-plus";
+  String QWEN_CODER_PLUS_LATEST = "qwen-coder-plus-latest";
+  String QWEN_CODER_PLUS_2024_11_06 = "qwen-coder-plus-2024-11-06";
+
+  String QWEN_CODER_TURBO = "qwen-coder-turbo";
+  String QWEN_CODER_TURBO_LATEST = "qwen-coder-turbo-latest";
+  String QWEN_CODER_TURBO_2024_09_19 = "qwen-coder-turbo-2024-09-19";
+
+  String QWQ_32B = "qwq-32b";
+  String QWQ_32B_PREVIEW = "qwq-32b-preview";
+
+  String QWEN3_235B_A22B = "qwen3-235b-a22b";
+  String QWEN3_32B = "qwen3-32b";
+  String QWEN3_30B_A3B = "qwen3-30b-a3b";
+  String QWEN3_14B = "qwen3-14b";
+  String QWEN3_8B = "qwen3-8b";
+  String QWEN3_4B = "qwen3-4b";
+  String QWEN3_1_7B = "qwen3-1.7b";
+  String QWEN3_0_6B = "qwen3-0.6b";
+
+  String QWEN2_5_14B_INSTRUCT_1M = "qwen2.5-14b-instruct-1m";
+  String QWEN2_5_7B_INSTRUCT_1M = "qwen2.5-7b-instruct-1m";
+  String QWEN2_5_72B_INSTRUCT = "qwen2.5-72b-instruct";
+  String QWEN2_5_32B_INSTRUCT = "qwen2.5-32b-instruct";
+  String QWEN2_5_14B_INSTRUCT = "qwen2.5-14b-instruct";
+  String QWEN2_5_7B_INSTRUCT = "qwen2.5-7b-instruct";
+  String QWEN2_5_3B_INSTRUCT = "qwen2.5-3b-instruct";
+  String QWEN2_5_1_5B_INSTRUCT = "qwen2.5-1.5b-instruct";
+  String QWEN2_5_0_5B_INSTRUCT = "qwen2.5-0.5b-instruct";
+
+  String QWEN2_5_MATH_72B_INSTRUCT = "qwen2.5-math-72b-instruct";
+  String QWEN2_5_MATH_7B_INSTRUCT = "qwen2.5-math-7b-instruct";
+  String QWEN2_5_MATH_1_5B_INSTRUCT = "qwen2.5-math-1.5b-instruct";
+
+  String QWEN2_5_CODER_32B_INSTRUCT = "qwen2.5-coder-32b-instruct";
+  String QWEN2_5_CODER_14B_INSTRUCT = "qwen2.5-coder-14b-instruct";
+  String QWEN2_5_CODER_7B_INSTRUCT = "qwen2.5-coder-7b-instruct";
+  String QWEN2_5_CODER_3B_INSTRUCT = "qwen2.5-coder-3b-instruct";
+  String QWEN2_5_CODER_1_5B_INSTRUCT = "qwen2.5-coder-1.5b-instruct";
+  String QWEN2_5_CODER_0_5B_INSTRUCT = "qwen2.5-coder-0.5b-instruct";
+
+  String QWEN2_57B_A14B_INSTRUCT = "qwen2-57b-a14b-instruct";
+  String QWEN2_72B_INSTRUCT = "qwen2-72b-instruct";
+  String QWEN2_7B_INSTRUCT = "qwen2-7b-instruct";
+  String QWEN2_1_5B_INSTRUCT = "qwen2-1.5b-instruct";
+  String QWEN2_0_5B_INSTRUCT = "qwen2-0.5b-instruct";
+
+  String QWEN1_5_110B_CHAT = "qwen1.5-110b-chat";
+  String QWEN1_5_72B_CHAT = "qwen1.5-72b-chat";
+  String QWEN1_5_32B_CHAT = "qwen1.5-32b-chat";
+  String QWEN1_5_14B_CHAT = "qwen1.5-14b-chat";
+  String QWEN1_5_7B_CHAT = "qwen1.5-7b-chat";
+  String QWEN1_5_1_8B_CHAT = "qwen1.5-1.8b-chat";
+  String QWEN1_5_0_5B_CHAT = "qwen1.5-0.5b-chat";
+
+  String CODEQWEN1_5_7B_CHAT = "codeqwen1.5-7b-chat";
+}
