7a7e70c4ec52a8ba4d981a18563ee50f2ee7a840
Tong Li <litongjava001@gmail.com>
2025-06-13 15:56:30 -1000

    add GEMINI_2_5_FLASH_PREVIEW_05_20

--- src/main/java/com/litongjava/gemini/GoogleGeminiModels.java
+++ src/main/java/com/litongjava/gemini/GoogleGeminiModels.java
@@ -53,6 +53,7 @@ public interface GoogleGeminiModels {
   String GEMINI_2_5_PRO_PREVIEW_03_25 = "gemini-2.5-pro-preview-03-25";
   String GEMINI_2_5_PRO_PREVIEW_05_06 = "gemini-2.5-pro-preview-05-06";
   String GEMINI_2_5_FLASH_PREVIEW_04_17 = "gemini-2.5-flash-preview-04-17";
+  String GEMINI_2_5_FLASH_PREVIEW_05_20 = "gemini-2.5-flash-preview-05-20";
 
   // Gemini Experimental
   String GEMINI_EXP_1206 = "gemini-exp-1206";
