ee2eaeb4d334fafdad85ab8c4f35a8ff462b64ce
Tong Li <litongjava001@gmail.com>
2025-07-06 15:57:32 -1000

    remove  GenerateException toString

--- src/main/java/com/litongjava/exception/GenerateException.java
+++ src/main/java/com/litongjava/exception/GenerateException.java
@@ -2,11 +2,9 @@ package com.litongjava.exception;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class GenerateException extends RuntimeException {
 
   private static final long serialVersionUID = -4841623914325044141L;
