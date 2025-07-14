23f1760ae563b7cf7a0c4594cd45ebe23f8c2aa8
Tong Li <litongjava001@gmail.com>
2025-07-13 18:09:13 -1000

    add code request

--- src/main/java/com/litongjava/linux/CodeRequest.java
+++ src/main/java/com/litongjava/linux/CodeRequest.java
@@ -3,16 +3,23 @@ package com.litongjava.linux;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 public class CodeRequest {
   private Long id;
   private String code;
   private Integer timeout;
 
   public CodeRequest(String code) {
+    this.code = code;
+  }
+
+  public CodeRequest(long id, String code) {
+    this.id = id;
     this.code = code;
   }
 }
