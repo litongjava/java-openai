8e1f367f8d9c94a7dbb1da2aec0a68a3bd971b38
Tong Li <litongjava001@gmail.com>
2025-08-31 12:41:34 -1000

    add tts platform

---
+++ src/main/java/com/litongjava/consts/UniTableName.java
@@ -0,0 +1,5 @@
+package com.litongjava.consts;
+
+public interface UniTableName {
+  String UNI_TTS_CACHE = "uni_tts_cache";
+}
---
+++ src/main/java/com/litongjava/tts/TTSPlatform.java
@@ -0,0 +1,10 @@
+package com.litongjava.tts;
+
+public interface TTSPlatform {
+  String volce = "volce";
+  String openai = "openai";
+  String fishaudio = "fishaudio";
+  String minimax = "minimax";
+  String kokoroEn = "kokoro_en";
+  String matchaEn = "matcha_en";
+}
