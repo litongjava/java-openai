923b806b0ec9ae911ddedca27f200598ef873234
Tong Li <litongjava001@gmail.com>
2025-12-10 20:55:20 -1000

    update FishAudioReference

--- src/main/java/com/litongjava/fishaudio/tts/FishAudioReference.java
+++ src/main/java/com/litongjava/fishaudio/tts/FishAudioReference.java
@@ -3,6 +3,6 @@ package com.litongjava.fishaudio.tts;
 public interface FishAudioReference {
 
   String English_Donald_J_Trump = "5196af35f6ff4a0dbf541793fc9f2157";
-  String Chinese_Lei_Jun = "5196af35f6ff4a0dbf541793fc9f2157";
+  String Chinese_Lei_Jun = "aebaa2305aa2452fbdc8f41eec852a79";
 
 }
