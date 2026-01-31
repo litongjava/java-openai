0c635f95fb5fb6a57c77cceb2a25f4717a9ada64
litongjava <litongjava001@gmail.com>
2026-01-31 09:03:20 +0800

    update PromptResult

--- src/main/java/com/litongjava/utils/PromptResult.java
+++ src/main/java/com/litongjava/utils/PromptResult.java
@@ -1,7 +1,26 @@
 package com.litongjava.utils;
 
-public interface PromptResult {
-  String YES = "yes";
-  String NO = "no";
-  String DONE = "done";
+import com.litongjava.tio.utils.hutool.StrUtil;
+
+public class PromptResult {
+  public static final String YES = "yes";
+  public static final String NO = "no";
+
+  public static final String TRUE = "true";
+  public static final String FALSE = "false";
+
+  public static final String DONE = "done";
+
+  public static boolean isTrue(String input) {
+    if (StrUtil.isBlank(input)) {
+      return false;
+    }
+    input = input.trim().toLowerCase();
+    if (YES.equals(input)) {
+      return true;
+    } else if (TRUE.equals(input)) {
+      return true;
+    }
+    return false;
+  }
 }
